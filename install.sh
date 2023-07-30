dd78614244e00b421e5e871f70d2ec04d63b10df
Nilsu Derinder <44870116+exababy@users.noreply.github.com>
2023-07-30 15:32:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,63 +5,12 @@ user="steam"
 IP="0.0.0.0"
 PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 CUSTOM_FILES="${CUSTOM_FOLDER:-custom_files}"
-if [ -f /etc/os-release ]; then
-	# freedesktop.org and systemd
-	. /etc/os-release
-	DISTRO_OS=$NAME
-	DISTRO_VERSION=$VERSION_ID
-elif type lsb_release >/dev/null 2>&1; then
-	# linuxbase.org
-	DISTRO_OS=$(lsb_release -si)
-	DISTRO_VERSION=$(lsb_release -sr)
-elif [ -f /etc/lsb-release ]; then
-	# For some versions of Debian/Ubuntu without lsb_release command
-	. /etc/lsb-release
-	DISTRO_OS=$DISTRIB_ID
-	DISTRO_VERSION=$DISTRIB_RELEASE
-elif [ -f /etc/debian_version ]; then
-	# Older Debian/Ubuntu/etc.
-	DISTRO_OS=Debian
-	DISTRO_VERSION=$(cat /etc/debian_version)
-else
-	# Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-	DISTRO_OS=$(uname -s)
-	DISTRO_VERSION=$(uname -r)
-fi
-
-# Download latest stop script
-curl --silent --output "stop.sh" "https://raw.githubusercontent.com/kus/csgo-modded-server/master/stop.sh" && chmod +x stop.sh
-
-# Check distrib
-if ! command -v apt-get &> /dev/null; then
-	echo "ERROR: OS distribution not supported... $DISTRO_OS $DISTRO_VERSION"
-	exit 1
-fi
-
-# Check root
-if [ "$EUID" -ne 0 ]; then
-	echo "ERROR: Please run this script as root..."
-	exit 1
-fi
 
 if [ -z "$PUBLIC_IP" ]; then
 	echo "ERROR: Cannot retrieve your public IP address..."
 	exit 1
 fi
 
-echo "Updating Operating System..."
-apt update -y -q && apt upgrade -y -q >/dev/null
-if [ "$?" -ne "0" ]; then
-	echo "ERROR: Updating Operating System..."
-	exit 1
-fi
-
-echo "Adding i386 architecture..."
-dpkg --add-architecture i386 >/dev/null
-if [ "$?" -ne "0" ]; then
-	echo "ERROR: Cannot add i386 architecture..."
-	exit 1
-fi
 cd /home/${user}/csgo/csgo/warmod/ && python3 -m http.server 80 </dev/null &>/dev/null &
 
 cd /home/${user}
